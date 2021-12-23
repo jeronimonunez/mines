fe8d35c0bdecb42742e7c86d35271986d868dc46
Jeronimo Nunez <jeronimonunez@gmail.com>
2021-12-23 10:22:06 -0300

    Update

---
+++ src/components/Icons/MineIcon.js
@@ -0,0 +1,7 @@
+import '../../styling/Icons/MineIcon.css';
+
+const MineIcon = () => {
+  return (<i className="icon-mine"><i></i></i>)
+};
+
+export default MineIcon;
---
+++ src/components/Icons/index.js
@@ -0,0 +1,1 @@
+export { default } from './MineIcon.js'
